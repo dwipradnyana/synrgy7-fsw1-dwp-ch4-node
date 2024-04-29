835dda94161f8d486bda3bb630e19be24ffa20f9
dwipradnyana <dwipradnyana68@gmail.com>
2024-04-29 19:46:19 +0800

    feat: add fileSystem

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+// const os = require("os");
+const fs = require("fs");
 const kubus = require("./kubus");
 const balok = require("./balok");
 const prismaSegitiga = require("./prismaSegitiga");
 
+const volumeKubus = kubus.volume(3);
+console.log("Volume Kubus: ", volumeKubus);
 
-const volumeKubus = kubus.volume(3)
-console.log("Volume Kubus: ", volumeKubus)
+const luasPermukaanKubus = kubus.luasPermukaan(3);
+console.log("Luas Permukaan Kubus: ", luasPermukaanKubus);
 
-const luasPermukaanKubus = kubus.luasPermukaan(3)
-console.log("Luas Permukaan Kubus: ", luasPermukaanKubus)
+const volumeBalok = balok.volume(2, 3, 4);
+console.log("Volume Balok: ", volumeBalok);
 
-const volumeBalok = balok.volume(2,3,4)
-console.log("Volume Balok: ", volumeBalok)
+const luasPermukaanBalok = balok.luasPermukaan(2, 3, 4);
+console.log("Luas Permukaan Balok: ", luasPermukaanBalok);
 
-const luasPermukaanBalok = balok.luasPermukaan(2,3,4)
-console.log("Luas Permukaan Balok: ", luasPermukaanBalok)
+const volumePrismaSegitiga = prismaSegitiga.volume(2, 3, 4);
+console.log("Volume Prisma Segitiga: ", volumePrismaSegitiga);
 
-const volumePrismaSegitiga = prismaSegitiga.volume(2,3,4)
-console.log("Volume Prisma Segitiga: ", volumePrismaSegitiga)
+const luasPermukaanPrismaSegitiga = prismaSegitiga.luasPermukaan(2, 3, 4);
+console.log("Luas Permukaan Prisma Segitiga: ", luasPermukaanPrismaSegitiga);
 
-const luasPermukaanPrismaSegitiga = prismaSegitiga.luasPermukaan(2,3,4)
-console.log("Luas Permukaan Prisma Segitiga: ", luasPermukaanPrismaSegitiga)
+fs.writeFileSync("./result.txt", `Volume Balok: ${volumeBalok}`, "utf-8");
